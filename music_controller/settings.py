42e469acf950d5a9b9d26504fbc0573a5ffd871c
kiwi <41132322+whs146@users.noreply.github.com>
2022-02-05 00:21:16 +0800

    django

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
-    'frontend.apps.FrontendConfig',
+    # 'frontend.apps.FrontendConfig',
     'spotify.apps.SpotifyConfig'
 ]
 
