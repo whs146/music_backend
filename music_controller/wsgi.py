a77bbb53afbdcce7b6f9f03a059c1b01396752b6
kiwi <41132322+whs146@users.noreply.github.com>
2022-02-05 01:46:15 +0800

    django

--- music_controller/wsgi.py
+++ music_controller/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for music_controller project.
+WSGI config for student_management_system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
