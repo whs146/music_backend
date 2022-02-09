4136869d17f7d98375a91fa99556fde9cd5ccabf
kiwi <41132322+whs146@users.noreply.github.com>
2022-02-09 23:35:38 +0800

    django

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 db=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
